b1fc88768bfb93b999ae86226c3a4d2ac23e76a1
Ingeodev <ingeodev@gmail.com>
2025-09-21 01:58:35 -0500

    (FIX): rename repository

--- src/app/features/map/map.component.ts
+++ src/app/features/map/map.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { GeoJSONSourceComponent, LayerComponent, MapComponent, MarkerComponent } from '@maplibre/ngx-maplibre-gl';
+import { Component, computed, inject } from '@angular/core';
+import { MapComponent, GeoJSONSourceComponent, LayerComponent  } from '@maplibre/ngx-maplibre-gl';
+
+import { appStore } from '../../state/app-store';
+import {bbox} from '@turf/bbox';
+import  { FeatureCollection, Geometry } from 'geojson'
 
 @Component({
   selector: 'app-map',
-  imports: [MapComponent],
+  imports: [MapComponent, GeoJSONSourceComponent, LayerComponent ],
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
 export class Map {
+  store = inject(appStore)
+  geometries = computed(() => {
+    return this.store.geometries().map(geom => JSON.parse(geom))
+  })
 
+  extend = computed(() => {
+    if(this.geometries().length > 0){
+          const extend = bbox(this.geojson())
+    return extend as [number, number, number, number]
+    }
+
+    return [-76, 3.4, -76.5, 3.5] as [number, number, number, number]
+  })
+
+  geojson = computed<FeatureCollection>(() => ({
+    type: 'FeatureCollection',
+    features: this.geometries().map((geom, i) => ({
+      type: 'Feature',
+      geometry: geom as Geometry,  // geom ya es un objeto de tipo Geometry (Point, LineString, Polygon...)
+      properties: { id: i }
+    }))
+  }));
 }
